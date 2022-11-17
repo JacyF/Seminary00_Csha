2cb499bee8df80b7812319c31fc1b241b035f50f
JacyF <brunoloouuiis@gmail.com>
2022-11-18 02:34:01 +0300

    added task06

---
+++ Seminary01/task06/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine(" Please introduce a number ");
+
+int number;
+number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(number);
+
+if ( number % 2 == 0 )
+{
+   Console.WriteLine(" yes "); 
+}
+else
+{
+     Console.WriteLine("no "); 
+}
+
+
