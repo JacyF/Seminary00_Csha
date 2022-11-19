9d885ba08c33c26ada8959b705415847c40ff336
JacyF <brunoloouuiis@gmail.com>
2022-11-19 03:38:47 +0300

    adde task10

---
+++ Seminary02/task10/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа. 
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine(" Please , introduce three-digit number 'XXX' ");
+int number , a ;
+number = int.Parse(Console.ReadLine());
+
+if ( number >= 100 ) 
+{
+     if  (number < 1000) 
+     {
+        a = (number % 100) / 10 ;
+        Console.Write( number);
+        Console.Write(" --> ");
+        Console.WriteLine( a ) ;
+     }
+     else
+     {
+        Console.Write(" the introduced number was : ");
+        Console.WriteLine( number );
+     }
+}
+else
+{
+    Console.Write(" the introduced number was : ");
+    Console.WriteLine( number );
+}
+
+
+
