df7caa73657938b948e758dbf45dbb255d53150d
JacyF <brunoloouuiis@gmail.com>
2022-11-20 21:58:28 +0300

    added task13

---
+++ Seminary02/task13/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+// что третьей цифры нет. Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine(" Please , introduce a number  ");
+int number, a, b;
+number = int.Parse(Console.ReadLine());
+
+if (number < 100) Console.WriteLine(" Do not have the 3 digit ");
+    else if ((number >= 100) && (number < 1000))
+    {
+        a = number % 10;
+        Console.WriteLine(a);
+    }
+    else if ((number >= 1000) && (number < 10000))
+    {
+        a = (number % 100) / 10;
+        Console.WriteLine(a);
+    }
+    else if ((number >= 10000) && (number < 100000))
+    {
+        a = (number % 1000) / 100;
+        Console.WriteLine(a);
+    }
+else
+{
+    Console.WriteLine(" Please , try another number  ");
+}    
+
+
