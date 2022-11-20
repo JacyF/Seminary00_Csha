22dbbc43fbb3796bb3c97da2df9a2556769d078e
JacyF <brunoloouuiis@gmail.com>
2022-11-20 22:11:21 +0300

    added resol task15

---
+++ Seminary02/task15/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Introduce a number ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day == 1 ) Console.WriteLine($" {day} --> Not ");
+else if (day == 2 ) Console.WriteLine($" {day} --> Not ");
+else if (day == 3 ) Console.WriteLine($" {day} --> Not ");
+else if (day == 4 ) Console.WriteLine($" {day} --> Not ");
+else if (day == 5 ) Console.WriteLine($" {day} --> Not ");
+else if (day == 6 ) Console.WriteLine($" {day} --> Yes ");
+else if (day == 7 ) Console.WriteLine($" {day} --> Yes ");
+else  Console.WriteLine($" {day} --> do not existe ");
